77fc43a098b8bde5aebb6110beb9fd8a5cba9df3
jfmaes <jfmaes@nviso.be>
2021-04-28 23:10:19 +0200

    initial upload

---
+++ DELEGATES.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerveGas
+{
+    class DELEGATES
+    {
+        /*https://docs.microsoft.com/en-us/windows/win32/devnotes/etweventwritefull*/
+        /*
+         *EtwEventWriteFull(
+           __in REGHANDLE RegHandle,
+           __in PCEVENT_DESCRIPTOR EventDescriptor,
+           __in USHORT EventProperty,
+           __in_opt LPCGUID ActivityId,
+           __in_opt LPCGUID RelatedActivityId,
+           __in ULONG UserDataCount,
+           __in_ecount_opt(UserDataCount) PEVENT_DATA_DESCRIPTOR UserData
+           );
+         */
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate STRUCTS.NtStatus EtwEventWriteFull(ulong regHandle, IntPtr eventDescriptorPointer, ushort eventProperty, IntPtr activityId, IntPtr relatedActivityId, uint userDataCount, IntPtr userData);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate STRUCTS.NtStatus EtwEventWrite(ulong regHandle, IntPtr eventDescriptor, uint userDataCount, IntPtr userData);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MinHook;
+
+namespace NerveGas
+{
+    class Program
+    {
+        [DllImport("ntdll.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern STRUCTS.NtStatus EtwEventWrite(ulong regHandle, IntPtr eventDescriptor, uint userDataCount, IntPtr userData);
+        public static DELEGATES.EtwEventWriteFull ogEtwEventWriteFull;
+        public static DELEGATES.EtwEventWrite ogEtwEventWrite;
+
+        public static STRUCTS.NtStatus EtwEventWriteFull_Hook(ulong regHandle, IntPtr eventDescriptorPointer, ushort eventProperty, IntPtr activityId, IntPtr relatedActivityId, uint userDataCount, IntPtr userData)
+        {
+            Console.WriteLine("ETWEventWriteFull CALLED");
+            return (STRUCTS.NtStatus)ogEtwEventWriteFull(regHandle, eventDescriptorPointer, eventProperty, activityId, relatedActivityId, userDataCount, userData);
+        }
+        public static STRUCTS.NtStatus EtwEventWrite_Hook(ulong regHandle, IntPtr eventDescriptor, uint userDataCount, IntPtr userData)
+        {
+            Console.WriteLine("ETWEventWrite CALLED");
+            return (STRUCTS.NtStatus)ogEtwEventWrite(regHandle, eventDescriptor, userDataCount, userData);
+        }
+
+        public static void enableETWHook()
+        {
+            using (HookEngine engine = new HookEngine())
+            {
+                ogEtwEventWriteFull = engine.CreateHook("ntdll.dll", "EtwEventWriteFull", new DELEGATES.EtwEventWriteFull(EtwEventWriteFull_Hook));
+                ogEtwEventWrite = engine.CreateHook("ntdll.dll", "EtwEventWrite", new DELEGATES.EtwEventWrite(EtwEventWrite_Hook));
+                engine.EnableHooks();
+            }
+        }
+
+        public static void sayHello()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main(string[] args)
+        {
+            enableETWHook();
+            Console.WriteLine("hook got placed");
+            EtwEventWrite(0, IntPtr.Zero, 0, IntPtr.Zero);
+            sayHello();
+            byte[] assemblyBytes = File.ReadAllBytes(@"C:\Users\Jean\source\repos\ReflectionDemoApp\bin\Release\ReflectionDemoApp.exe");
+            Assembly.Load(assemblyBytes);
+            Console.ReadKey();
+        }
+    }
+}
